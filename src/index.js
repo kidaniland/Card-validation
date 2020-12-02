0da05221d13eafdaf9b3772d82d88d4c833352b2
kidaniland <mdcvillas@gmail.com>
2020-12-02 15:34:41 -0300

    Terminadas funciones isValid y maskify en Validados.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import validator from './validator.js';
 
 console.log(validator);
 
+//ELEMENTOS DE HTML
+let inputCardNumber = document.getElementById('inputNumber');
+const bValidate = document.getElementById('btn-validate');
+
+//EVENTO BOTON VALIDAR
+/*bValidate.addEventListener('click', function(){
+    let cardNumber = inputCardNumber.value;
+    console.log("BOTON", cardNumber);
+    validator.isValid(creditCardNumber);
+    validator.maskify(creditCardNumber);
+})*/
