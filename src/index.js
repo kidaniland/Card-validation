2cf5e52d6479f31947725c01270442fbd87e2e65
kidaniland <mdcvillas@gmail.com>
2020-12-03 19:14:08 -0300

    JEST listo, Validator dentro de objeto listo, css con display para respuesta none/block listo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,41 @@ import validator from './validator.js';
 console.log(validator);
 
 //ELEMENTOS DE HTML
-let inputCardNumber = document.getElementById('inputNumber');
+const inputCardNumber = document.getElementById('inputNumber');
 const bValidate = document.getElementById('btn-validate');
+const sectionCard = document.getElementById('card');
+const divFalse = document.getElementById('wrong');
+const divTrue = document.getElementById('correct');
+
+/*//EVENTO DEL TECLADO
+let replaceNumber = document.getElementById('inputNumber');
+replaceNumber.addEventListener('keyup', function(e){
+    let valueInput = e.target.value; //valor de lo tecleado en el input recibido mediante el evento
+    console.log("VALUE INPUt", valueInput);
+    arrayNumber.push(valueInput);
+    if(valueInput.length < 13){
+        inputCardNumber.value = valueInput.replace(/\d/g, '*');
+    }
+    else{
+        valueInput = e.target.value;
+    }
+    console.log("ARRAY>>",arrayNumber);
+})*/
 
 //EVENTO BOTON VALIDAR
-/*bValidate.addEventListener('click', function(){
-    let cardNumber = inputCardNumber.value;
+bValidate.addEventListener('click', function(){
+    let creditCardNumber = inputCardNumber.value;
     console.log("BOTON", cardNumber);
     validator.isValid(creditCardNumber);
     validator.maskify(creditCardNumber);
-})*/
+//CONDICION PARA MOSTRAR RESULTADOS
+    if(isValid(creditCardNumber)){
+        sectionCard.style.display = "block";
+        divTrue.style.display = "block";
+    }
+    else{
+        sectionCard.style.display = "block";
+        divFalse.style.display = "block";
+    }
+})
+
--- src/validator.js
+++ src/validator.js
@@ -1,76 +1,68 @@
-
+/*//PRUEBA EN CONSOLA
+let num = "5592022120486706"
+isValid(num);
+maskify(num);*/
 
 const validator = {
-   //METODOS ISVALID y MASKIFY
-   /*validator.isValid(creditCardNumber);
-   validator.maskify(creditCardNumber);*/
-
-};
-
 //retornar un BOOLEAN dependiendo si es válida o no la tarjeta
-/*function isValid(number){
-   console.log("esto llega en number>>>", number);
-   let totalOdd = 0;
-   let totalEven = 0;
-   let add = 0;
-   let a = 0;
-   let b = 0;
-
-   for (let i=0; i<number.length; i+=2){
-      totalOdd = parseInt(number[i])*2;
-      console.log("IMPARES", totalOdd);
-      if(totalOdd >9){
-         let double = (totalOdd % 10)+1;
-         a+=double;
-         console.log("AAAA>>>", a);
+   isValid: function(number){
+      //console.log("esto llega en number>>>", number);
+      let totalOdd = 0;
+      let totalEven = 0;
+      let add = 0;
+      let a = 0;
+      let b = 0;
+   
+      for (let i=0; i<number.length; i+=2){
+         totalOdd = parseInt(number[i])*2;
+         //console.log("IMPARES", totalOdd);
+         if(totalOdd >9){
+            let double = (totalOdd % 10)+1;
+            a+=double;
+            //console.log("AAAA>>>", a);
+         }
+         else{
+            b += totalOdd;
+            //console.log("BBBBB>>>",b);
+         }
+         totalOdd = a+b;
+      } 
+      //console.log("Total IMPARES", totalOdd);
+   
+      for (let i=1; i<number.length; i+=2){
+         totalEven += parseInt(number[i]); 
+      }
+      //console.log("TOTAL PAR", totalEven);
+   
+      add = totalEven+totalOdd;
+      //console.log("SUMA DE TODO", add);
+   
+      if(add % 10 ===0){
+         return true;
       }
       else{
-         b += totalOdd;
-         console.log("BBBBB>>>",b);
+         return false;
       }
-      totalOdd = a+b;
-   } 
-   console.log("Total IMPARES", totalOdd);
-
-   for (let i=1; i<number.length; i+=2){
-      totalEven += parseInt(number[i]); 
-   }
-   console.log("TOTAL PAR", totalEven);
-
-   add = totalEven+totalOdd;
-   console.log("SUMA DE TODO", add);
-
-   if(add % 10 ===0){
-      return true;
-   }
-   else{
-      return false;
-   }
-}*/
+   },
 
 //retorna String donde todos menos los últmos 4 dígitos serán un #
-function maskify(cardN){
-   console.log("llega a MASKIFY", cardN);
-   let string = "";
-   let numReplace = "#";
-   let cantDigits = 0;
-   for (let i=0; i<cardN.length; i++){
-      cantDigits++
-      if(cantDigits < 13){
-         string += numReplace;
+   maskify: function(cardN){
+      //console.log("llega a MASKIFY", cardN);
+      let string = "";
+      let numReplace = "#";
+      let cantDigits = 0;
+      for (let i=0; i<cardN.length; i++){
+         cantDigits++
+         if(cantDigits < 13){
+            string += numReplace;
+         }
+         else{
+            string += cardN[i];
+         }
       }
-      else{
-         string += cardN[i];
-      }
+      //console.log("STRING CON CAMBIO>>", string);
+      return string
    }
-   console.log("STRING CON CAMBIO>>", string);
-   // return string
-}
-
-//PRUEBA EN CONSOLA
-let num = "5592022120486706"
-//isValid(num);
-maskify(num);
-
+};
 
 export default validator;
